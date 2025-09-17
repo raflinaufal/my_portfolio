68d10d13b4144e10c0a1d529e7000a649f5b4754
raflinaufal <raflinaufalalief204@gmail.com>
2025-09-17 22:32:11 +0700

    add vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,3 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -14,10 +8,6 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-  },
-  webpack: (config) => {
-    config.resolve.alias["@/components"] = path.join(__dirname, "components");
-    return config;
   },
 };
 
