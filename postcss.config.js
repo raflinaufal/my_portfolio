74974ab7cc3767b6dd7fd13d111d3d805869c36b
raflinaufal <raflinaufalalief204@gmail.com>
2025-09-17 22:22:39 +0700

    add vercel

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+/** @type {import('postcss-load-config').Config} */
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}, // 👈 tambahin autoprefixer biar sesuai best practice
+  },
+};
